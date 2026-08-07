8d4077ece02dc3452bdc3a137eaa518acfcc0b98
DarkKnight9979 <mohamed2929mostafa2929@gmail.com>
2026-08-07 22:47:28 +0300

    Update __init__.py

--- tvdatafeed/__init__.py
+++ tvdatafeed/__init__.py
@@ -1,16 +1,1 @@
-class Interval:
-    in_1_minute = "1m"
-    in_5_minute = "5m"
-    in_15_minute = "15m"
-    in_30_minute = "30m"
-    in_1_hour = "1h"
-    in_4_hour = "4h"
-    in_daily = "1D"
-    in_weekly = "1W"
-    in_monthly = "1M"
-
-class TvDatafeed:
-    def __init__(self, username=None, password=None):
-        pass
-    def get_hist(self, *args, **kwargs):
-        return None
+from tvdatafeed.main import TvDatafeed, Interval
