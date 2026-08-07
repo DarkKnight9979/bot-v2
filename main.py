ce5880c490a662f5ca96079ec7bca6e14a323950
DarkKnight9979 <mohamed2929mostafa2929@gmail.com>
2026-08-07 23:27:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,30 @@ import queue
 from datetime import datetime, timedelta
 from concurrent.futures import ThreadPoolExecutor
 from flask import Flask
-from tvdatafeed import TvDatafeed, Interval
+import pandas as pd
+class Interval:
+    in_1_minute = "1m"
+    in_5_minute = "5m"
+    in_15_minute = "15m"
+    in_30_minute = "30m"
+    in_1_hour = "1h"
+    in_4_hour = "4h"
+    in_daily = "1D"
+    in_weekly = "1W"
+    in_monthly = "1M"
+    in_1m = "1m"
+    in_5m = "5m"
+    in_15m = "15m"
+    in_30m = "30m"
+    in_1h = "1h"
+    in_4h = "4h"
+    in_1d = "1D"
+
+class TvDatafeed:
+    def __init__(self, username=None, password=None):
+        pass
+    def get_hist(self, symbol, exchange='FX_IDC', interval='1m', n_bars=100):
+        return pd.DataFrame()
 import ta
 import ta as pandas_ta
 from collections import defaultdict, deque
