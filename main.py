cf93aaa819b92c74b88755d8a43f9aa2a0df202e
DarkKnight9979 <mohamed2929mostafa2929@gmail.com>
2026-08-07 22:42:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import queue
 from datetime import datetime, timedelta
 from concurrent.futures import ThreadPoolExecutor
 from flask import Flask
-from tvdatafeed import TvDatafeed, Interval
+from tvdatafeed import Interval
 import ta
 import ta as pandas_ta
 from collections import defaultdict, deque
