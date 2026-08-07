220ffadcb8492ba5ecda4f41fba2d36bb2394000
DarkKnight9979 <mohamed2929mostafa2929@gmail.com>
2026-08-07 23:02:56 +0300

    Update __init__.py

--- tvdatafeed/__init__.py
+++ tvdatafeed/__init__.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 class Interval:
+    # المسميات بالطريقتين لمنع أي خطأ
     in_1_minute = "1m"
     in_5_minute = "5m"
     in_15_minute = "15m"
@@ -10,6 +11,14 @@ class Interval:
     in_daily = "1D"
     in_weekly = "1W"
     in_monthly = "1M"
+    
+    in_1m = "1m"
+    in_5m = "5m"
+    in_15m = "15m"
+    in_30m = "30m"
+    in_1h = "1h"
+    in_4h = "4h"
+    in_1d = "1D"
 
 class TvDatafeed:
     def __init__(self, username=None, password=None):
